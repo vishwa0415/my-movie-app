3aa1cad9558e3a2ab0f79747005c64f1b55f8014
Vishwa Gautham <132287231+vishwa0415@users.noreply.github.com>
2026-09-02 03:06:49 +0530

    main app development

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,90 @@
 import React from 'react';
+import {useState,useEffect} from 'react';
+import {useDebounce} from 'react-use';
+import Search from'./Components/Search.jsx';
+import Spinner from'./Components/Spinner.jsx';
+import MovieCard from'./Components/MovieCard.jsx';
+
+const API_BASE_URL='https://api.themoviedb.org/3';
+const API_KEY= import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS={
+  method:'GET',
+  headers:{
+    accept:'application/json',
+    Authorization:`Bearer ${API_KEY}`
+  }
+}
 const App=()=>{
+    const[searchTerm,setSearchTerm]=useState('');
+  const[errorMessage, setErrorMessage]=useState('');
+  const[movieList,setMovieList]=useState([]);
+  const[isLoading,setIsLoading]=useState(false);
+  const[debouncedSearchTerm, setDebouncedSearchTerm]=useState('');
+  useDebounce(()=>{setDebouncedSearchTerm(searchTerm)},500,[searchTerm]);
+  const fetchMovies= async(query="")=>{
+    setIsLoading(true);
+    setErrorMessage('');
+    try{
+      const endpoint= query 
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : 
+      `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response=await fetch(endpoint,API_OPTIONS);
+      //alert(response);
+      if(!response.ok){
+        throw new Error('failed to fetch data');
+      }
+      const data=await response.json();
+      if(data.Response === 'false'){
+        setErrorMessage(data.Error || 'Failed to fetch moview.');
+        setMovieList([]);
+        return;
+
+      }
+      setMovieList(data.results || []);
+    }catch(error){
+        console.error(`Error fetching movies: ${error}`);
+        setErrorMessage('Error fetching moviews.Please try again Later.');
+    }finally{
+      setIsLoading(false);
+    }
+  }
+  useEffect(()=>{
+    fetchMovies(debouncedSearchTerm);
+
+  },[debouncedSearchTerm]);
+
   return(
     <main>
       <div className="pattern"/>
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner"/>
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle 
           </h1>
          
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
-        <p>Search</p>
+        
+        <section className="all-movies">
+          <h2 className="mt-[40px]">
+            All Movies
+          </h2>
+          {isLoading ?
+          (<Spinner />)
+          :errorMessage ?
+          (<p className="text-red-500"> {errorMessage}</p>)
+          :(
+            <ul>
+              {movieList.map((mo)=>(
+               <MovieCard key={mo.id} movie={mo}/>
+              ))}
+            </ul>
+          )
+}
+            
 
+        </section>
       </div>
       
     </main>
